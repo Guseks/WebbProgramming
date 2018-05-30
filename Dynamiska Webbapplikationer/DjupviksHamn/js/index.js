89735f84328499c642642cbaa62a215a939d3ce6
Gustaf <gusek93@gmail.com>
2018-05-30 11:43:27 +0200

    Små fix till styling samt tagit bort article taggen för bilderna för att ge en godkänd validering utan att en rubrik finns. Kommenterat min lösning i javascript filen, samt diskuterat tillgänglighet i ett dokument.

--- Dynamiska Webbapplikationer/DjupviksHamn/js/index.js
+++ Dynamiska Webbapplikationer/DjupviksHamn/js/index.js
@@ -32,3 +32,17 @@
   }
 
 })();
+
+/*--------- Kommentar till min lösning---------
+Jag anser att min sajt till stora delar är produktionsredo och utformad enligt den designspecifikation jag fick. 
+Designen och layout känns väl anpassad och sajten andas rätt atmosfär. 
+Det tydliga området som jag upplever skulle kunna förbättras är implementeringen av Javascript koden och hur menyn fäster sig vid toppen.
+Vilket mer är ett tekniskt perspektiv eftersom att den nuvarande menyn är fullt användbar, men den som gillar detaljer kan se att sajten "hackar" till när menyn fäster.
+Dessutom skulle menyn även kunna fastna på mindre skärmar om det är önskvärt, men i nuläget är inte det aktuellt eftersom att det gör zoomning helt omöjligt på mindre skärm.
+En annan möjlig förbättring är att göra navigeringsmenyn för mindre skärmar mer användarvänlig och så att den tar upp mindre plats på sajten.
+Detta skulle kunna göras genom att dölja menyn i en meny som visas när användaren vill. Någon form av navigeringsknapp som är vanlig på mobilsajter.
+
+En annan sak som inte är implementerad är att formuläret i nuläget inte gör något med den data som användaren skickar in. Den skickar inte informationen någonstans på något lämpligt sätt.
+För att implementera detta behövs ytterligare teknisk utveckling i form av javascript kod som utvecklar denna funktionalitet och exempelvis skickar informationen som fylldes i till en mailadress
+formaterat på ett lämpligt sätt.
+*/
